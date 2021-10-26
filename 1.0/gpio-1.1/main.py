f8197af56e36ab53b6a28966d8ab9e8b69292a12
gitofthere <gnelsondirect@outlook.com>
2021-10-26 09:50:19 -0800

    reorg

--- 1.0/gpio-1.1/main.py
+++ 1.0/gpio-1.1/main.py
@@ -1,5 +1,5 @@
 '''
-
+src reference https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
 '''
 import RPi.GPIO as GPIO
 import time
@@ -20,23 +20,16 @@ GPIO.setup(u1t, GPIO.OUT)
 GPIO.setup(u1e, GPIO.IN)
  
 def ultradist(ut, ue):
-    # set Trigger to HIGH
     GPIO.output(ut, True)
-    # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(ut, False)
     StartTime = time.time()
     StopTime = time.time()
-    # save StartTime
     while GPIO.input(ue) == 0:
         StartTime = time.time()
-    # save time of arrival
     while GPIO.input(ue) == 1:
         StopTime = time.time()
-    # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
     return distance
 
@@ -45,9 +38,10 @@ if __name__ == '__main__':
     try:
         while True:
             dist1 = ultradist(u1t, u1e)
-            print ("Measured Distance = %.1f cm" % dist)
+            dist2 = ultradist(u2t, u2e)
+            dist3 = ultradist(u3t, u3e)
+            print (dist1, " , ", dist2 , " , ", dist3)
             time.sleep(1)
-        # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
