8f3f7bef07bb189c14cc38d52124c9c9a78433f9
Gatlin Nelson <gnelsondev@gmail.com>
2021-12-01 12:11:04 -0900

    update

--- code_references/distance.py
+++ code_references/distance.py
@@ -1,51 +1,54 @@
 '''
-src reference https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
+https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
+
+https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
 '''
+#Libraries
 import RPi.GPIO as GPIO
 import time
- 
+import math
+
+
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
- 
-# Ultrasonic Sensor Pins
-u1t = 17
-u1e = 18
-u2t = 27
-u2e = 22
-u3t = 23
-u3e = 24
 
-#set GPIO direction (IN / OUT)
+#set GPIO Pins
+u1t = 4
+u1e = 17
+u2t = 18
+u2e = 27
+u3t = 22
+u3e = 23
+
 GPIO.setup(u1t, GPIO.OUT)
 GPIO.setup(u1e, GPIO.IN)
 GPIO.setup(u2t, GPIO.OUT)
 GPIO.setup(u2e, GPIO.IN)
 GPIO.setup(u3t, GPIO.OUT)
 GPIO.setup(u3e, GPIO.IN)
- 
-def ultradist(ut, ue):
-    GPIO.output(ut, True)
+
+def distance(GPIO_TRIGGER, GPIO_ECHO):
+    GPIO.output(GPIO_TRIGGER, True)
     time.sleep(0.00001)
-    GPIO.output(ut, False)
+    GPIO.output(GPIO_TRIGGER, False)
     StartTime = time.time()
     StopTime = time.time()
-    while GPIO.input(ue) == 0:
+    while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
-    while GPIO.input(ue) == 1:
+    while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
     TimeElapsed = StopTime - StartTime
     distance = (TimeElapsed * 34300) / 2
     return distance
 
-
 if __name__ == '__main__':
     try:
         while True:
-            dist1 = ultradist(u1t, u1e)
-            dist2 = ultradist(u2t, u2e)
-            dist3 = ultradist(u3t, u3e)
-            print (dist1, " | ", dist2 , " | ", dist3)
-            time.sleep(1)
+            dist1 = distance(u1t, u1e)
+            dist2 = distance(u2t, u2e)
+            dist3 = distance(u3t, u3e)
+            print (dist1, " | ", dist2, " | ", dist3)
+            time.sleep(0.3)
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
