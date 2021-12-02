24bd270861cd62052d546790ccff027ba9f2824d
Gatlin Nelson <gnelsondev@gmail.com>
2021-12-01 15:02:47 -0900

    update

--- robot.py
+++ robot.py
@@ -24,17 +24,23 @@ https://tutorials-raspberrypi.com/raspberry-pi-servo-motor-control/
 https://embeddedcircuits.com/raspberry-pi/tutorial/controlling-a-servo-motor-with-raspberry-pi-tutorial
 '''
 
+'''
+Checklist:
+'''
 
 import RPi.GPIO as gp
 import time
 from time import sleep
-gp.setmode(gp.BOARD)
+import sys
+
+gp.setmode(gp.BCM)
+
 
 logfile = "log.txt"
 
 
 pwm_mode_bool = 0 # zero or one
-
+servo_range_of_motion = 180
 
 #----------------------------------------------------------------------------------------------------
 #                                       Pins
@@ -43,12 +49,12 @@ pwm_mode_bool = 0 # zero or one
 # Ultrasonic Sensor Pins
 # BCM - 4,17,18,27,22,23
 
-u1t = 7
-u1e = 11
-u2t = 12
-u2e = 13
-u3t = 15
-u3e = 16
+u1t = 4
+u1e = 17
+u2t = 18
+u2e = 27
+u3t = 22
+u3e = 23
 
 # Motor Pins
 # BCM - 12,16,20,21 
@@ -63,13 +69,13 @@ m4 = 40
 
 serv1 = 18
 
-# Set Distance Pinmodes
-gp.setup(u1t, gp.OUT)
-gp.setup(u1e, gp.IN)
-gp.setup(u2t, gp.OUT)
-gp.setup(u2e, gp.IN)
-gp.setup(u3t, gp.OUT)
-gp.setup(u3e, gp.IN)
+# Ultrasonic Sensor Pinmodes
+GPIO.setup(u1t, GPIO.OUT)
+GPIO.setup(u1e, GPIO.IN)
+GPIO.setup(u2t, GPIO.OUT)
+GPIO.setup(u2e, GPIO.IN)
+GPIO.setup(u3t, GPIO.OUT)
+GPIO.setup(u3e, GPIO.IN)
 
 # Set Motor Pinmodes
 gp.setup(m1, gp.OUT)
@@ -81,35 +87,22 @@ gp.setup(m4, gp.OUT)
 gp.setup(servoPIN, gp.OUT)
 
 #----------------------------------------------------------------------------------------------------
-#                                       Distance Functions
+#                                       Distance Function
 #----------------------------------------------------------------------------------------------------
  
-def ultradist(ut, ue):
-    gp.output(ut, True)
+def distance(tg, eh):
+    GPIO.output(tg, True)
     time.sleep(0.00001)
-    gp.output(ut, False)
+    GPIO.output(tg, False)
     StartTime = time.time()
     StopTime = time.time()
-    while gp.input(ue) == 0:
+    while GPIO.input(eh) == 0:
         StartTime = time.time()
-    while gp.input(ue) == 1:
+    while GPIO.input(eh) == 1:
         StopTime = time.time()
     TimeElapsed = StopTime - StartTime
     distance = (TimeElapsed * 34300) / 2
     return distance
-
-def gd():
-     #   Front
-     gd.dist1 = ultradist(u1t, u1e)
-     #   Rear Left
-     gd.dist2 = ultradist(u2t, u2e)
-     #   Rear Right
-     gd.dist3 = ultradist(u3t, u3e)
-
-     #check if at or closer than 10cm
-     gd.idist1 = dist1 <= 10
-     gd.idist2 = dist2 <= 10
-     gd.idist3 = dist3 <= 10
 
 #----------------------------------------------------------------------------------------------------
 #                                       Drive Functions
@@ -122,40 +115,36 @@ dirrihw="Turning Right..."
 
 
 # these need to be tested for the L298N
+
+# Normal Mode
+
 def forw():
     gp.output(m1,gp.HIGH)
     gp.output(m2,gp.LOW)
     gp.output(m3,gp.HIGH)
     gp.output(m4,gp.LOW)
+    print(dirforw)
 
 def bakw():
     gp.output(m1,gp.LOW)
     gp.output(m2,gp.HIGH)
     gp.output(m3,gp.LOW)
     gp.output(m4,gp.HIGH)
+    print(dirbakw)
 
 def lefw():
     gp.output(m1,gp.LOW)
     gp.output(m2,gp.HIGH)
     gp.output(m3,gp.HIGH)
     gp.output(m4,gp.LOW)
+    print(dirlefw)
 
 def rihw():
     gp.output(m1,gp.HIGH)
     gp.output(m2,gp.LOW)
     gp.output(m3,gp.LOW)
     gp.output(m4,gp.HIGH)
-    
-def pwm_forw():
-
-
-def pwm_bakw():
-
-
-def pwm_lefw():
-
-
-def pwm_rihw():
+    print(dirrihw)
 
 def stopw():
     gp.output(m1,gp.LOW)
@@ -163,60 +152,147 @@ def stopw():
     gp.output(m3,gp.LOW)
     gp.output(m4,gp.LOW)
 
+# PWM
+
+def drive_pwm(val1, val2, val3, val4):
+    pwm1.ChangeDutyCycle(val1)
+    pwm2.ChangeDutyCycle(val2)
+    pwm1.ChangeDutyCycle(val3)
+    pwm2.ChangeDutyCycle(val4)
+
 #----------------------------------------------------------------------------------------------------
 #                                       Logic Functions
 #----------------------------------------------------------------------------------------------------
 
 # Scan with Servo
 
+def servo_scan():
+    stopw()
 
-# See which rear sensor is closer to an object
+# sweep forwards
 
-def chicken(chick):
-    dist.gd()
-    left = gd.dist2
-    right = gd.dist3
-    if left > right:
-        lefw()
-    elif right > left:
+    for i in (0,60):
+        # sweep servo
+        dist1 = distance(u1t, u1e)
+        dist2 = distance(u2t, u2e)
+        dist3 = distance(u3t, u3e)
+        # average the distance
+        av1 = ""
+    for i in (61,120):
+        # sweep servo
+        dist1 = distance(u1t, u1e)
+        dist2 = distance(u2t, u2e)
+        dist3 = distance(u3t, u3e)
+        # average the distance
+        av2 = ""
+    for i in (121,180):
+        # sweep servo
+        dist1 = distance(u1t, u1e)
+        dist2 = distance(u2t, u2e)
+        dist3 = distance(u3t, u3e)
+        # average the distance
+        av3 = ""
+
+    if av1 > max(av2, av3):
         rihw()
-    else:
-        if chick==1:
-            rihw()
-        elif chick==2:
-            lefw()
-
-# See if sensor is 10cm or less away from object
-
-def ifclose(): 
-    dist.gd()
-
-    if dist.gd.idist1 == True:
+    elif av2 > max(av1, av3): 
         bakw()
-    if dist.gd.idist2 == True:
-        chicken(1) 
-    if dist.gd.idist3 == True:
-        chicken(2)
-    log()
-    clean()
+    elif av3 > max(av1, av2):
+        lefw()
+
+# sweep backwards
+
+    for i in (180,0):
+        # sweep servo
 
 
 
-def clean():
-    gp.cleanup()
+
+# If less than 10, and if one rear sensor is closer than the other
+
+def dir_opt_normal(dist1, dist2, dist3):
+    if dist1 <= 10:
+        servo_scan()
+    else:
+        forw()
+
+    if dist2 <= 10 or dist3 <= 10:
+        if dist2 < dist3:
+            rihw()
+        elif dist3 < dist2:
+            lefw()
+        else:
+            forw()
+
+# If less than 10, and if one rear sensor is closer than the other
+
+def dir_opt_pwm(dist1, dist2, dist3):
+    if dist1 <= 10:
+        servo_scan()
+    else:
+        forw()
+
+    if dist2 <= 10 or dist3 <= 10:
+        if dist2 < dist3:
+            rihw()
+        elif dist3 < dist2:
+            lefw()
+        else:
+            forw()
+
+def normal_run(): 
+    
+    while True:
+
+        dist1 = distance(u1t, u1e)
+        dist2 = distance(u2t, u2e)
+        dist3 = distance(u3t, u3e)
+
+        dir_opt(dist1, dist2, dist3)
+
+def pwm_run(): 
+
+    init_freq = 100
+    dc = 0
+    pwm1 = gp.PWM(m1,init_freq)  
+    pwm2 = gp.PWM(m2,init_freq)
+    pwm3 = gp.PWM(m3,init_freq)  
+    pwm4 = gp.PWM(m4,init_freq)
+
+    pwm1.start(dc)
+    pwm2.start(dc)
+    pwm3.start(dc)
+    pwm4.start(dc)
+
+    while True:
+
+        dist1 = distance(u1t, u1e)
+        dist2 = distance(u2t, u2e)
+        dist3 = distance(u3t, u3e)
+
+        dir_opt_pwm(dist1, dist2, dist3):
 
 #----------------------------------------------------------------------------------------------------
-#                                       Main Function
+#                                       Main Functions
 #----------------------------------------------------------------------------------------------------
 
 def log():
 
 def main():
-    if pwm_mode_bool==0:
-        print("Running in Normal mode")
-    elif pwm_mode_bool==1:
-        print("Running in PWM mode")
-    else:
-        print("pwm_mode_bool not set to zero or one...")
+    try:
+        if pwm_mode_bool==0:
+                print("Running in Normal mode")
+                normal_run()
+            elif pwm_mode_bool==1:
+                print("Running in PWM mode")
+                pwm_run()
+            else:
+                print("pwm_mode_bool not set to zero or one...")
+                sys.exit()
+
+    except KeyboardInterrupt:
+        stopw():
+        GPIO.cleanup()
+        print("Robot Stopped")
 
 main()
